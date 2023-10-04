3d024dfca3f63a9f9ef3c63a2b8649967363dfb7
makhshafhaider1 <makhshaf@contrivers.dev>
2023-10-04 17:01:05 +0500

    hello world api

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 // Express Initialize
 const app = express();
-app.get('/hello_world', (req,res)=>{
+app.get('/', (req,res)=>{
     res.send('Hello World');
     })
-    
+
 const port = 8000;
 app.listen(port,()=> {
 console.log('listen port 8000');
